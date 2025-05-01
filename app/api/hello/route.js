278f7a79162b4ea52b5a8287eccd7ce0fcce07e4
ynafey <ynafey@gmail.com>
2025-04-30 22:54:19 -0400

    First commit

---
+++ app/api/hello/route.js
@@ -0,0 +1,8 @@
+export async function GET(request) {
+
+	const stage = process.env.STAGE;
+	return new Response(JSON.stringify({ message: "Hello from App Router API. Stage is " + stage }), {
+		status: 200,
+		headers: { "Content-Type": "application/json" },
+	});
+}
